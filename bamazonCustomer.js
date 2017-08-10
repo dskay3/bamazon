945c57b3b699e5e55d1dcea7f5e8302b61a3409e
Daniel <dskay3@gmail.com>
2017-08-09 22:26:33 -0400

    Added code to terminate when conditional is not met. Changed name for inquirer to reduce confusion.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,81 +5,88 @@ var chalk = require('chalk');
 
 const dashes = "----------------------------";
 
-// Connect with database
+// Parameters to connect with DB
 var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: "root",
-    password: "Kihyun12!",
-    database: "bamazon"
+	host: 'localhost',
+	port: 3306,
+	user: "root",
+	password: "Kihyun12!",
+	database: "bamazon"
 });
 
+// Connects SQL DB with Node
 connection.connect(function (error) {
-    if (error) throw error;
-    initialRun();
+	if (error) throw error;
+	initialRun();
 });
 
 // Display and perform actions
 function initialRun() {
-    var query = "SELECT * FROM products";
+	var query = "SELECT * FROM products";
 
-    // Executes query
-    connection.query(query, function (error, result) {
-        console.log(chalk.green.bold("\nCurrent Products in Bamazon"));
-        console.log(chalk.green.bold(dashes));
+	// Executes query
+	connection.query(query, function (error, result) {
+		console.log(chalk.green.bold("\nCurrent Products in Bamazon"));
+		console.log(chalk.green.bold(dashes));
 
-        for (var i = 0; i < result.length; i++) {
-            console.log(chalk.bold(result[i].id + " | ") + result[i].product_name + " ---- $" + chalk.red(result[i].price));
-        }
+		for (var i = 0; i < result.length; i++) {
+			console.log(chalk.bold(result[i].id + " | ") + result[i].product_name + " ---- $" + chalk.red(result[i].price));
+		}
 
-        console.log(chalk.green(dashes));
-        custPurchase();
-    });
+		console.log(chalk.green(dashes));
+		custPurchase();
+	});
 };
 
-// Search function
+// Customer purchase function
 function custPurchase() {
-    inquirer
-        .prompt({
-            name: "options",
-            type: "list",
-            message: "Would you like to make a purchase?: ",
-            choices: [
-                "Yes",
-                "No"
-            ]
-        })
-        .then(function (answer) {
-            if (answer.options === "Yes") {
-                inquirer
-                    .prompt([
-                        {
-                            name: "buy",
-                            type: "input",
-                            message: "Please enter the ID of the product you would like to buy: ",
-                            validate: function (value) {
-                                if (isNaN(value) === false) {
-                                    return true;
-                                }
-                                return false;
-                            }
-                        },
-                        {
-                            name: "numberOfUnits",
-                            type: "input",
-                            message: "How many would you like to buy?: ",
-                            validate: function (value) {
-                                if (isNaN(value) === false) {
-                                    return true;
-                                }
-                                return false;
-                            }
-                        }
-                    ])
-                    .then(function (answer) {
-                        console.log(answer.buy + " " + answer.numberOfUnits);
-                    })
-                }
-            })
-
+	// Prompts that ask if the customer wants to make a purchase
+	inquirer
+		.prompt({
+			name: "options",
+			type: "list",
+			message: "Would you like to make a purchase?: ",
+			choices: [
+				"Yes",
+				"No"
+			]
+		})
+		.then(function (answer) {
+			// Executes when yes is selected
+			if (answer.options === "Yes") {
+				// Prompts that ask what item to buy and the number to purchase
+				inquirer
+					.prompt([
+							{
+								name: "buyID",
+								type: "input",
+								message: "Please enter the ID of the product you would like to buy: ",
+								validate: function (value) {
+									if (isNaN(value) === false) {
+											return true;
+									}
+									return false;
+								}
+							},
+							{
+								name: "numUnits",
+								type: "input",
+								message: "How many would you like to buy?: ",
+								validate: function (value) {
+									if (isNaN(value) === false) {
+											return true;
+									}
+									return false;
+								}
+							}
+					])
+					.then(function (answer) {
+						purchaseItem(answer.buyID, numUnits)
+					})
+			}
+			// Terminates the connection since it is not needed anymore	
+			else {
+				connection.end();
+			}
+	})
 }
